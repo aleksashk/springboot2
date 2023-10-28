e46cb20def342242083bb9bf59798e0ea59cb6a2
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-28 19:18:25 +0300

    @RequestHeader

--- src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
+++ src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
@@ -1,16 +1,16 @@
 package by.flameksandr.spring.boot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/test")
-    public String test(@RequestParam String a,
-                       @RequestParam String b,
-                       @RequestParam String c) {
+    public String test(@RequestHeader String a,
+                       @RequestHeader String b,
+                       @RequestHeader String c) {
 
         return a + " " + b + " " + c;
     }
