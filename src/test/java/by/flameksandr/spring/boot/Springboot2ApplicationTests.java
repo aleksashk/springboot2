f6a8492954931dfd2a059a3bd08da0d2dfa178fa
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-28 18:41:09 +0300

    Initial commit

---
+++ src/test/java/by/flameksandr/spring/boot/Springboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
